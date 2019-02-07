950854fa8580968f7906d14818344d0819e83105
pantazescuvlad <pantazescu.vlad@gmail.com>
2019-02-07 10:47:46 +0200

    adding display function

---
+++ cg.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById("window");
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+var ctx = canvas.getContext("2d");
+
+function display() {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(10, 10, 1, 1);
+ }
