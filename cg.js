1310cd985088c9d5dfe5d84a152cab9483e67615
pantazescuvlad <pantazescu.vlad@gmail.com>
2019-02-07 14:39:36 +0200

    Added random dots

--- cg.js
+++ cg.js
@@ -3,7 +3,16 @@ var canvasWidth = canvas.width;
 var canvasHeight = canvas.height;
 var ctx = canvas.getContext("2d");
 
+var nrOfDots = 100;//number of dots that will be drawn on the canvas 
+
 function display() {
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(10, 10, 1, 1);
+    
+    for(let i = 0; i < nrOfDots; ++i){
+        let x = Math.floor(Math.random() * canvasWidth);
+        let y = Math.floor(Math.random() * canvasHeight);
+
+        ctx.fillRect(x, y, 1, 1);
+    }
  }
+
